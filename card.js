ae9113441c07d83f8b306087e3ca50205a23f220
mohandaggupati <42297026+mohandaggupati@users.noreply.github.com>
2019-02-12 12:56:42 +0530

    today

---
+++ card.js
@@ -0,0 +1,17 @@
+(function(){
+  function loadJson(file,callback){
+    var rawFile =new XMLHttpRequest();
+rawFile.overrideMimeType("application/json");
+rawFile.open("GET",file,true);
+rawFile.onreadystatechange =function(){
+if(rawFile.readyState ===4 & rawFile.status == "200"){
+        callback(rawFile.responseText)
+}
+  }
+rawFile.send(null);
+}
+loadJson("data.json",function (text){
+var data =JSON.parse(text);
+console.log(data);
+});
+})
